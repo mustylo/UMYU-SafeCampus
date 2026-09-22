82d995cdcd943d4a606ee66281e021e42c865367
Mustapha Umar <almustaphaumarnagwandu@gmail.com>
2026-09-22 23:13:26 +0100

    feat: connect Flask app to PostgreSQL

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,19 @@
+import os
+
+from dotenv import load_dotenv
 from flask import Flask
+
+from app.extensions import db
+
+load_dotenv()
+
 
 def create_app():
     app = Flask(__name__)
+
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+
+    db.init_app(app)
 
     @app.get("/")
     def home():
